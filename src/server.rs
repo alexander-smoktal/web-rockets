7c45a1b34bd366495aae7bc25681209f313e8bcb
Alexander Smoktal <alexandr.smoktal@lge.com>
2015-11-11 18:19:00 +0200

    Minor code cleanup
    Change-Id: I8bbb27d80ad4c2a57c8ea2aaa6e9546c3ec59998

--- src/server.rs
+++ src/server.rs
@@ -6,13 +6,18 @@ use mio::*;
 
 const SERVER: Token = Token(0);
 
+struct TokenFactory(usize);
+impl TokenFactory {
+    fn next_token(&mut self) -> Token { self.0 += 1; Token(self.0) }
+}
+
 pub struct WebSocketServer<T, C> where T: WebSocketHandler<C> {
     handler: T,                                         // User handler which implements callbacks
     phantom: marker::PhantomData<C>,                    // I need to use this to calm down the compiler
     listener: tcp::TcpListener,                         // Listens all incoming connections
 
     clients: collections::HashMap<Token, (C, tcp::TcpStream)>, // List af all clients
-    client_tokens: usize                                       // Counter to assign tokens to clients
+    tokens: TokenFactory                                       // Counter to assign tokens to clients
 }
 
 impl<T, C> Handler for WebSocketServer<T, C> where T: WebSocketHandler<C> {
@@ -21,25 +26,23 @@ impl<T, C> Handler for WebSocketServer<T, C> where T: WebSocketHandler<C> {
 
     fn ready(&mut self, main_loop: &mut EventLoop<Self>, token: Token, events: EventSet) {
         match token {
-            // Got new connection. Let's add it to the list of clients and notify a user
+            // Got new connection. Lets add it to the list of clients and notify a user
             SERVER => {
                 match self.listener.accept() {
-                    Ok(client) => {
-                        self.register_client(client, main_loop);
-                    },
+                    Ok(client) => { self.register_client(client, main_loop); },
                     Err(e) => { println!("Error accepting a client: {}", e) }
                 }
             }
-            ref t => {
-                // Client have disconnected
+            ref token => {
+                // Client has been disconnected
                 // TODO: Handle error
                 if events.is_error() || events.is_hup() {
-                    self.disconnect_client(token, main_loop);
+                    self.disconnect_client(*token, main_loop);
                     return;
                 }
 
-                // Client sent a message
-                match self.clients.get_mut(t) {
+                // Got a message from a client
+                match self.clients.get_mut(token) {
                     Some(&mut (ref mut client, ref mut stream)) => {
                         let ref mut buffer = [0; 1024];
 
@@ -55,7 +58,7 @@ impl<T, C> Handler for WebSocketServer<T, C> where T: WebSocketHandler<C> {
 }
 
 impl<T, C> WebSocketServer<T, C> where T: WebSocketHandler<C> + Sync {
-    fn new(handler: T, addr: &'static str) -> io::Result<WebSocketServer<T, C>> {
+    fn new(handler: T, addr: &'static str) -> io::Result<()> {
         let formated_addr = net::SocketAddr::from_str(addr).unwrap();
         let listener = try!(tcp::TcpListener::bind(&formated_addr));
 
@@ -64,26 +67,26 @@ impl<T, C> WebSocketServer<T, C> where T: WebSocketHandler<C> + Sync {
             phantom: marker::PhantomData,
             listener:listener,
             clients: collections::HashMap::new(),
-            client_tokens: 2
+            tokens: TokenFactory(1)
         };
 
         let mut main_loop = try!(EventLoop::<Self>::new());
         try!(main_loop.register(&result.listener, SERVER));
         try!(main_loop.run(&mut result));
 
-        return Ok(result)
+        return Ok(())
     }
 
     fn register_client(&mut self, client: Option<tcp::TcpStream>, main_loop: &mut EventLoop<Self>) {
         if let Some(client) = client {
-            self.client_tokens += 1;
+            let token = self.tokens.next_token();
 
-            match main_loop.register(&client, Token(self.client_tokens)) {
+            match main_loop.register(&client, token) {
                 Err(e) => { println!("Error registering a client: {}", e) },
                 _ => {
                     let addr = format!("{}", client.peer_addr().unwrap());
 
-                    let _ = self.clients.insert(Token(self.client_tokens),
+                    let _ = self.clients.insert(token,
                                                 (self.handler.on_connect(addr), client));
                 }
             }
@@ -104,7 +107,7 @@ impl<T, C> WebSocketServer<T, C> where T: WebSocketHandler<C> + Sync {
 }
 
 pub trait WebSocketHandler<C>: Sized + marker::Sync {
-    fn listen(self, addr: &'static str) -> io::Result<WebSocketServer<Self, C>> {
+    fn listen(self, addr: &'static str) -> io::Result<()> {
         return WebSocketServer::new(self, addr)
     }
 
